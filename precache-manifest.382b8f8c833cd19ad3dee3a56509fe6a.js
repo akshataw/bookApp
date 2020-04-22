e290450697239be6df49be0f174da908ae87ca65
akshataw <wagheakshu@gmail.com>
2020-04-22 16:51:40 +0530

    Updates

---
+++ precache-manifest.382b8f8c833cd19ad3dee3a56509fe6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbbef32adab14ad83dada448e95453c",
+    "url": "/bookApp/index.html"
+  },
+  {
+    "revision": "5983f9d2e3d06fbfa18b",
+    "url": "/bookApp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fd68651946e5cf64ab2e",
+    "url": "/bookApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5983f9d2e3d06fbfa18b",
+    "url": "/bookApp/static/js/2.5350cf4f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bookApp/static/js/2.5350cf4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd68651946e5cf64ab2e",
+    "url": "/bookApp/static/js/main.ad441c03.chunk.js"
+  },
+  {
+    "revision": "778926c48e982867c6cb",
+    "url": "/bookApp/static/js/runtime-main.a499c572.js"
+  }
+]);
